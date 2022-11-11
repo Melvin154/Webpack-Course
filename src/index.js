28ccc35f893f064d33cda7f42cbaf5bf4936f978
Melvin154 <melthomas2202gmail.com>
2022-11-12 01:07:13 +0530

    changed spelling of webpack

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import generateJoke from "../src/generateJoke";
 import './style/main.scss';
 console.log(generateJoke());
 console.log('Hello');
-console.log("Working on Wenpack");
+console.log("Working on Webpack");
